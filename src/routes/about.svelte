fa7130afda0166a70bac6b3e14cf4a57a23ab778
nikosgpet <nikosgpet@gmail.com>
2022-02-02 20:36:06 +0100

    Initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<section>
+	{#each Array(15) as x}
+	  <h1>(new) scroll ⬇</h1>
+	{/each}
+
+	<a href="/" style="font-size:24px; padding-top:24px; padding-bottom:64px">Click on this link</a>
+</section>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,48 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	{#each Array(10) as x}
+	  <h1>scroll ⬇</h1>
+	{/each}
+
+	<a href="/about" style="font-size:24px; padding-top:24px; padding-bottom:64px">Click on this link</a>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
